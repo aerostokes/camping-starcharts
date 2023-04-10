78685ecbad481530f8323e96418e5f656d5f6906
Rhonda Stokes <rhonda@aerostokes.com>
2023-04-10 13:33:33 -0700

    add pseudo-code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,49 @@
-// Collect and store user's search input and date
-    // If a park name, search for exact or close matches
-    // If state code, search for XX number of random sites in that state
+var npsAPIkey = "FOj8NLPQk3pjXfnLbAPTKd1psYcMq5EqEmMc72QR"
+var astroAPIkey = ""
+var hamBtn = document.getElementById("ham-btn");
+var searchSection = document.querySelector(".search");
+var campSearchInput = document.getElementById("camp-search");
+var datePickerInput = document.getElementById("datepicker");
+var searchBtn = document.getElementById("search-btn");
+var favToggleBtn = document.getElementById("fav-toggle");
+var resultsUl = document.getElementById("results");
+
+var campSection = document.getElementById("camp");
+var campNameEl = document.getElementById("camp-name");
+var chartImg = document.getElementById("chart");
+var infoAside = document.getElementById("info");
+var campURL = document.getElementById("camp-url");
+var infoListUl = document.getElementById("info-list");
+var aboutSection = document.querySelector(".about");
+var stateCodeArr = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
+
+
+
+// On page load, populate favorites from localStorage (if any)
+
+// Collect and store campSearchInput and datePickerInput
 
 // Fetch from NPS API using search parameters
-    // Store the latitude/longitude of the found campsites for 
+    // If campSearchInput exists in stateCodeArr: call function stateSearch
+    // Otherwise, call function keywordSearch
+    // function stateSearch: randomly select starting point, pull 50 results, randomly return 5 result.
+    // function keywordSearch: use q search, return top 5
+
+// With the data returned:
+    // Check for physical address to prevent errors, add placeholder text if null 
+    // Store the lat/lon of the found campsites, 
+
+// Create and append to resultsUL
+
     
-// 
+// When user clicks on li's of resultsUl, feed lat/lon and datePickerInput to astronomy API
+    // Collect and store img URL and campground info
+
+// Update chartImg src and populate infoAside
 
 
 // If the user clicks fav-btn: 
-    // If checking the box, change the checkbox style and to "filled" and store camp-name, park-code, address, geo-coordinates in localStorage.
-    // 
+    // If checking the box, change the checkbox style and to "filled" and store camp-name, location, lat/lon in localStorage.
+    // If unchecking the box, remove from localStorage
     
+
